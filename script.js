bb9571a77d24d67cf044dad5227945189a38e104
utkarshtiwari2904-crypto <utkarshtiwari2904@gmail.com>
2026-07-02 21:17:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ setTimeout(() => {
     }, 100);
 
 }, 6000);
+for(let i=0;i<120;i++){
+
+const star=document.createElement("div");
+
+star.className="star";
+
+star.style.left=Math.random()*100+"vw";
+
+star.style.top=Math.random()*100+"vh";
+
+star.style.animationDelay=Math.random()*3+"s";
+
+document.getElementById("stars").appendChild(star);
+
+}
